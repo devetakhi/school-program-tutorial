a8dc3437b664f56ae3aa0b9340ad842491f09f79
devetakhi <doreczek@gmail.com>
2017-10-15 10:24:34 +0200

    Update and rename 08.html to 08.js

---
+++ 08.js
@@ -0,0 +1,38 @@
+function wypiszNaKonsole(x){
+  console.log(x);
+}
+  
+          class Student {
+
+            constructor(imie, nazwisko, liczbaJedynek, liczbaSzostek, liczbaPiatek, id) {
+                this.imie = imie;
+                this.nazwisko = nazwisko;
+                this.id = id;
+                this.liczbaJedynek = liczbaJedynek;
+                this.liczbaSzostek = liczbaSzostek;
+                this.liczbaPiatek = liczbaPiatek;
+            }
+            
+
+            obliczSrednia() {
+                return (this.liczbaJedynek * 1 + this.liczbaSzostek * 6 + this.liczbaPiatek * 5) / (this.liczbaJedynek + this.liczbaSzostek + this.liczbaPiatek);
+            }
+
+
+            pokazSzczegoly() {
+                wypiszNaKonsole("Student Id: " + this.id);
+                wypiszNaKonsole("Nazwisko: " + this.nazwisko + " " + this.imie);
+                wypiszNaKonsole("Średnia: " + this.obliczSrednia());
+
+            }
+        }
+
+        let st1 = new Student("John", "Smith", 15, 85, 79, 90);
+        let st2 = new Student("Hannah", "Turner", 23, 75, 80, 82);
+        let st3 = new Student("Kevin", "White", 4, 93, 89, 90);
+        let st4 = new Student("Rose", "Taylor", 11, 55, 63, 45);
+
+        st1.pokazSzczegoly();
+        st2.pokazSzczegoly();
+        st3.pokazSzczegoly();
+        st4.pokazSzczegoly();
